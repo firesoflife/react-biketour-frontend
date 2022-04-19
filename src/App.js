905115104720a54f3cbf3c6dd2bb449352d4473a
firesoflife <bryangoertz@gmail.com>
2022-04-18 22:14:54 -0400

    fixed map render - map marker still broken

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.scss';
+import './leaflet/leaflet.css';
 // Components
 import Navbar from './components/Navbar';
 // Pages
@@ -14,31 +15,31 @@ import ContestSubmissions from './pages/ContestSubmissions';
 import MobileNav from './components/MobileNav';
 
 function App() {
-	const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-	const toggle = () => {
-		setIsOpen(!isOpen);
-	};
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
 
-	return (
-		<div className='App'>
-			<Navbar toggle={toggle} />
-			<MobileNav isOpen={isOpen} toggle={toggle} />
-			<Switch>
-				<Route path='/' exact component={Home} />
-				<Route path='/map' exact component={RaceMap} />
-				<Route path='/racers' exact component={RacerProfiles} />
-				<Route
-					path='/racers/profile/:id'
-					exact
-					component={SingleRacerProfile}
-				/>
-				<Route path='/gallery' exact component={ImagePage} />
-				<Route path='/contest' exact component={ContestForm} />
-				<Route path='/submissions' exact component={ContestSubmissions} />
-			</Switch>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <Navbar toggle={toggle} />
+      <MobileNav isOpen={isOpen} toggle={toggle} />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/map' exact component={RaceMap} />
+        <Route path='/racers' exact component={RacerProfiles} />
+        <Route
+          path='/racers/profile/:id'
+          exact
+          component={SingleRacerProfile}
+        />
+        <Route path='/gallery' exact component={ImagePage} />
+        <Route path='/contest' exact component={ContestForm} />
+        <Route path='/submissions' exact component={ContestSubmissions} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/RaceMap.jsx
+++ src/pages/RaceMap.jsx
@@ -4,75 +4,79 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { IoBicycleOutline } from 'react-icons/io5';
 import axios from 'axios';
 
-const RaceMap = props => {
-	const [position, setPosition] = useState([]);
+const RaceMap = (props) => {
+  const [position, setPosition] = useState([]);
 
-	useEffect(() => {
-		axios
-			// .get('http://localhost:4000/racers')
-			.get('https://peaceful-atoll-77526.herokuapp.com/racers')
-			.then(resp => {
-				setPosition(resp.data);
-				// console.log(resp.data);
-			})
-			.catch(resp => console.log(resp));
-	}, [position.length]);
+  useEffect(() => {
+    axios
+      // .get('http://localhost:4000/racers')
+      .get('https://peaceful-atoll-77526.herokuapp.com/racers')
+      .then((resp) => {
+        setPosition(resp.data);
+        // console.log(resp.data);
+      })
+      .catch((resp) => console.log(resp));
+  }, [position.length]);
 
-	const marker = position.map(racerPosition => {
-		return (
-			<Fragment>
-				<Marker
-					key={racerPosition.id}
-					position={[racerPosition.lat, racerPosition.lng]}
-				>
-					<Popup>
-						<div className='popup__container'>
-							<div className='popup__profile--img'>
-								<div className='profile-container'>
-									<div className='popup-icon'>
-										<IoBicycleOutline />
-										<p>
-											Img <br /> Placeholder
-										</p>
-									</div>
-								</div>
-							</div>
-							<div className='popup__details'>
-								<h3>
-									{racerPosition.f_name} {racerPosition.l_name}
-								</h3>
-								<Link
-									to={`/racers/profile/${racerPosition.id}`}
-									className='btn'
-								>
-									See{' '}
-									<span className='btn-link'> {racerPosition.f_name}'s' </span>
-									Full Profile
-								</Link>
-							</div>
-						</div>
-					</Popup>
-				</Marker>
-			</Fragment>
-		);
-	});
+  const marker = position.map((racerPosition) => {
+    return (
+      <Fragment>
+        <Marker
+          key={racerPosition.id}
+          position={[racerPosition.lat, racerPosition.lng]}
+        >
+          <Popup>
+            <div className='popup__container'>
+              <div className='popup__profile--img'>
+                <div className='profile-container'>
+                  <div className='popup-icon'>
+                    <IoBicycleOutline />
+                    <p>
+                      Img <br /> Placeholder
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className='popup__details'>
+                <h3>
+                  {racerPosition.f_name} {racerPosition.l_name}
+                </h3>
+                <Link
+                  to={`/racers/profile/${racerPosition.id}`}
+                  className='btn'
+                >
+                  See{' '}
+                  <span className='btn-link'> {racerPosition.f_name}'s' </span>
+                  Full Profile
+                </Link>
+              </div>
+            </div>
+          </Popup>
+        </Marker>
+      </Fragment>
+    );
+  });
 
-	return (
-		<div className='map-container'>
-			<h1 className='title'>Track Your Favourite Riders</h1>
-			<MapContainer
-				center={[40.0149856, -105.2705456]}
-				zoom={13}
-				scrollWheelZoom={true}
-			>
-				<TileLayer
-					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-				/>
-				{marker}
-			</MapContainer>
-		</div>
-	);
+  return (
+    <div className='map-container'>
+      <h1 className='title'>Track Your Favourite Riders</h1>
+      <MapContainer
+        center={[40.0149856, -105.2705456]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        {/* <TileLayer
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        /> */}
+        {marker}
+      </MapContainer>
+    </div>
+  );
 };
 
 export default RaceMap;
